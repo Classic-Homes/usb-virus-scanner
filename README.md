03b6147f96851496e63af038a025b51c15e9e265
Chris Loidolt <loidolt@gmail.com>
2023-09-22 09:25:28 -0600

    Fix permissions and display issue

--- README.md
+++ README.md
@@ -24,8 +24,16 @@ This script requires the following:
 
 Clone the repository
 
+Make 'usb_scanner.py' executable 
+`sudo chmod -u+x usb-virus-scanner/usb_scanner.py`
+
 Install dependencies
 `sudo apt install python3 python3-pyudev dbus-x11`
+
+Allow User to run ClamAV without password:
+`sudo visudo`
+Add the following to the end of the file:
+`administrator ALL=(ALL) NOPASSWD: /usr/bin/clamscan`
 
 This script assumes you are using an account called 'administrator' with sudo permissions. This script is not intended to be used on a daily driver machine or workstation, but rather a testing environment that is capable of being wiped and rebuilt in the event of an uncontained infection.
 
--- usb_scanner.py
+++ usb_scanner.py
@@ -5,43 +5,50 @@ import time
 import subprocess
 from pyudev import Context, Monitor, MonitorObserver
 
+
 def on_device_event(device):
     """Callback function to be called when a device is added."""
     if device.action == 'add' and device.get('ID_FS_TYPE') in ['vfat', 'ntfs', 'exfat', 'ext4']:
         dev_path = device.get('DEVNAME')
         fs_type = device.get('ID_FS_TYPE')
         print(f"Device Added: {dev_path}, FS Type: {fs_type}")
-        
+
         # Wait for the device to be mounted.
         time.sleep(2)
-        
+
         # Find the mount point.
         mount_point = None
-        result = subprocess.run(['df', '--output=target', dev_path], capture_output=True, text=True)
+        result = subprocess.run(
+            ['df', '--output=target', dev_path], capture_output=True, text=True)
         if result.returncode == 0:
             lines = result.stdout.strip().split('\n')
             if len(lines) > 1:
-                mount_point = lines[1]  # The second line contains the mount point.
-        
+                # The second line contains the mount point.
+                mount_point = lines[1]
+
         if mount_point:
             print(f"USB Device Added: {dev_path} at {mount_point}")
             scan_device(mount_point)
         else:
-            print(f"Could not find the mount point for {dev_path}. Skipping scan.")
+            print(
+                f"Could not find the mount point for {dev_path}. Skipping scan.")
+
 
 def scan_device(dev_path):
     """Function to scan the device using ClamAV"""
     if os.geteuid() != 0:
         print("You need to have root privileges to run the scan.")
         return
-    
+
     # Prepare the command
-    command = f'gnome-terminal --command "bash -c \'clamscan -r --remove {dev_path}; echo Scan Completed! Press Enter to close.; read\'"'
+    command = f'gnome-terminal --command "bash -c \'sudo clamscan -r --remove {dev_path}; echo Scan Completed! Press Enter to close.; read\'"'
+
     try:
         print(f"Running: {command}")
         subprocess.run(command, shell=True, check=True)
     except Exception as e:  # Catching all exceptions for better debugging
         print(f"Error occurred while scanning: {str(e)}")
+
 
 def main():
     """Main function to setup device monitor"""
@@ -49,7 +56,8 @@ def main():
     monitor = Monitor.from_netlink(context)
     monitor.filter_by(subsystem='block')
 
-    observer = MonitorObserver(monitor, callback=on_device_event, name='observer')
+    observer = MonitorObserver(
+        monitor, callback=on_device_event, name='observer')
     observer.daemon = False
     observer.start()
 
@@ -58,6 +66,7 @@ def main():
         observer.join()
     except KeyboardInterrupt:
         print("Exiting...")
+
 
 if __name__ == "__main__":
     main()
